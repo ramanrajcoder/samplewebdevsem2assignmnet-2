efab02289d5909d4e5ae7b6465a4c10cb826b6d1
raman raj <ramanrajshrivastava@gmail.com>
2026-02-23 14:12:21 +0530

    second commit

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault()
     console.log(city.value)
 })
+
+
+//https://api.openweathermap.org/data/2.5/weather?q=daltonganj&appid=0133cc5316757ac730cc46ae342334e4
+
+const API_KEY="0133cc5316757ac730cc46ae342334e4"
+const form=document.querySelector("#weather_from")
+
+form.addEventListener("submit",async(e)=>{
+    e.preventDefault()
+    console.log(city.value)
+    const data=city.value
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=daltonganj&appid=0133cc5316757ac730cc46ae342334e4`)
+    const weatherData=await response.json()
+    console.log("city",weatherData.name)
+    console.log("temperature",weatherData.main.temp-273).toFixed(1)
+})
